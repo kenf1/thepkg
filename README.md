95882e13c783f31f9a5f15ff6996d4dcfda281a8
kenf1 <103538771+kenf1@users.noreply.github.com>
2024-03-11 21:28:10 -0500

    Add functions

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## The Package: Rust edition
+## The Package: Rust edition <a href="https://www.rust-lang.org/"><img src="https://github.com/kenf1/RewriteInRust/blob/main/ref/rewrite.jpg?raw=true" align="right" height="250" /></a>
 
 [![Cargo Test](https://github.com/kenf1/thepkg/actions/workflows/cargo_test.yaml/badge.svg?branch=main)](https://github.com/kenf1/thepkg/actions/workflows/cargo_test.yaml)
 
-A collection of my commonly used Rust code chunks as functions.
+A collection of my frequently used Rust code.
--- src/iofn.rs
+++ src/iofn.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::fs;
+use std::fs::{self,ReadDir};
 use csv::Writer;
 use polars::prelude::*;
 
@@ -30,4 +30,77 @@ pub fn save_df(mut df: DataFrame,output_path: &str){
         .unwrap();
 
     println!("{} saved",output_path);
+}
+
+//store results as Vec<String>
+pub fn path_tree(paths: ReadDir,debug_print: bool) -> Vec<String>{
+    let mut path_vec: Vec<String> = Vec::new();
+
+    for path in paths{
+        if let Ok(temp) = path{
+            let path_str = temp
+                .path()
+                .display()
+                .to_string();
+            path_vec.push(path_str);
+        }
+    }
+
+    if debug_print == true{
+        println!("{:?}\n---\n",path_vec);
+    }
+
+    path_vec
+}
+
+//filter path: Vec<String>, not destructive
+pub fn paths_filter(paths : Vec<String>,excluded: Vec<&str>,debug_print: bool) -> Vec<String>{
+    let filt_paths: Vec<String> = paths
+        .iter()
+        .filter(|&element| !excluded.iter().any(|&exclusion| element.contains(exclusion)))
+        .cloned()
+        .collect();
+
+    if debug_print == true{
+        println!("Paths to remove target folder from:\n{:?}\n",filt_paths);
+    }
+
+    filt_paths
+}
+
+//rm all target folders
+pub fn loop_rm(paths: Vec<String>){
+    for item in paths{
+        let result = fs::remove_dir_all(
+            format!("{}/target",item.to_string())
+        );
+
+        //output outcome message
+        match result{
+            Ok(_) => println!("{}/target removed successfully.",item.to_string()),
+            Err(e) => eprintln!("Error: {} -> Skipped",e),
+        }
+    }
+
+    println!("\nComplete");
+}
+
+/*
+    wrapper function to rm all target folders from target_path
+    params:
+        excluded = Vec<str> to exclude from all found paths
+        print_allpaths = prints all subpaths if true
+        print_filtpaths = print all subpaths (after filter) if true
+*/
+pub fn rm_alltarget(target_path: &str,excluded: Vec<&str>,print_allpaths: bool,print_filtpaths: bool){
+    //store all paths
+    let origpath = fs::read_dir(target_path)
+        .unwrap();
+    let all_paths = path_tree(origpath,print_allpaths);
+
+    //rm misc paths (rm_target project excluded)
+    let filt_paths = paths_filter(all_paths,excluded,print_filtpaths);
+
+    //rm target folder for each path in filt_paths
+    loop_rm(filt_paths);
 }
