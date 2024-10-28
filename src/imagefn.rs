3bfbd4062a7afec3d03ff6f2acfee128f1b2c465
kenf1 <103538771+kenf1@users.noreply.github.com>
2024-10-28 09:48:43 -0500

    Rewrite imagefn + tests

--- src/imagefn.rs
+++ src/imagefn.rs
@@ -1,16 +1,18 @@
-use image::{self,DynamicImage,GenericImageView,ImageFormat};
+use image::{DynamicImage,GenericImageView,ImageFormat};
 use reqwest::blocking::get;
 use image::imageops::CatmullRom; //resize algorithm
 use bardecoder;
 
 //import image from local path (allows error handling)
-pub fn image_import(image_path: &str) -> Result<DynamicImage,Box<dyn std::error::Error>>{
-    let temp_img = image::open(image_path.to_string())?;
+pub fn image_import(image_path: &str) -> 
+Result<DynamicImage,Box<dyn std::error::Error>>{
+    let temp_img = image::open(image_path)?;
     Ok(temp_img)
 }
 
 //import image from url (allows error handling)
-pub fn image_from_url(url: &str) -> Result<DynamicImage,Box<dyn std::error::Error>>{
+pub fn image_from_url(url: &str) -> 
+Result<DynamicImage,Box<dyn std::error::Error>>{
     let img_bytes = get(url)?
         .bytes()?
         .to_vec();
@@ -20,24 +22,17 @@ pub fn image_from_url(url: &str) -> Result<DynamicImage,Box<dyn std::error::Erro
 }
 
 //determine image dimensions & resize if any >= 600 pixels
-pub fn image_dimensions(image: DynamicImage,orig_cutoff: u32,new_cutoff: u32) -> DynamicImage{
-    //set check & resize cutoffs
-    let (orig_cutoff,new_dim) = (orig_cutoff,new_cutoff);
-
+pub fn image_dimensions(
+    image: DynamicImage,orig_cutoff: u32,new_cutoff: u32
+) -> DynamicImage{
     let (width,height) = image.dimensions();
-    println!("Image dimensions are: {} pixels in width x {} pixels in height.",width,height);
+    println!("Image dimensions are: {} pixels in width x {} pixels in height.", width, height);
 
-    /*
-        resize image if width &/or height >= 600 pixels, else return original
-            - maintains orig aspect ratio
-            - uses CatmullRom filter (https://docs.rs/image/latest/image/imageops/enum.FilterType.html)
-    */
-    if (width >= orig_cutoff) | (height >= orig_cutoff){
+    if width >= orig_cutoff || height >= orig_cutoff {
         println!("Image width and/or height >= {} pixels -> will be resized, maintaining original aspect ratio.",orig_cutoff);
 
-        let new_image = DynamicImage::resize(&image,new_dim,new_dim,CatmullRom);
-        println!("Resized image dimensions: {:?}",new_image.dimensions());
-
+        let new_image = DynamicImage::resize(&image,new_cutoff,new_cutoff,CatmullRom);
+        println!("Resized image dimensions: {:?}", new_image.dimensions());
         new_image
     }else{
         image
@@ -45,74 +40,58 @@ pub fn image_dimensions(image: DynamicImage,orig_cutoff: u32,new_cutoff: u32) ->
 }
 
 //decode image
-pub fn image_decode(image: DynamicImage) -> String{
-    //setup decoder & storage vec
+pub fn image_decode(image: DynamicImage) -> 
+Result<String,Box<dyn std::error::Error>>{
     let decoder = bardecoder::default_decoder();
-    let mut decoded_items: Vec<String> = Vec::new();
 
-    //append to vec
-    let results = decoder.decode(&image);
-    for result in results{
-        if let Ok(decoded_item) = result{
-            decoded_items.push(decoded_item);
-        }
-    }
-
-    let qrcontent = decoded_items.join("");
-    qrcontent
+    let decoded_items: Vec<String> = decoder
+        .decode(&image)
+        .into_iter()
+        .filter_map(|result| result.ok())
+        .collect();
+    Ok(decoded_items.join(""))
 }
 
 //wrapper function for local image
-pub fn from_local(image_path: &str,orig_cutoff: u32,new_cutoff: u32) -> String{
-    match image_import(image_path){
-        Ok(qrcode) => {
-            //resize if nec
-            let tidy = image_dimensions(qrcode,orig_cutoff,new_cutoff);
+pub fn from_local(image_path: &str,orig_cutoff: u32,new_cutoff: u32) -> 
+Result<String,Box<dyn std::error::Error>>{
+    let qrcode = image_import(image_path)?;
 
-            //return qr content
-            let res = image_decode(tidy);
-            res
-        },
-        Err(err) => {
-            let res = String::from("Error: unable to decode QR code");
-            res
-        }
-    }
+    //resize if nec
+    let tidy = image_dimensions(qrcode,orig_cutoff,new_cutoff);
+
+    let res = image_decode(tidy)?;
+    Ok(res)
 }
 
 //wrapper function for remote image (same structure as from_local)
-pub fn from_remote(url: &str,orig_cutoff: u32,new_cutoff: u32) -> String{
-    match image_from_url(url){
-        Ok(qrcode) => {
-            //resize if nec
-            let tidy = image_dimensions(qrcode,orig_cutoff,new_cutoff);
-            
-            //return qr content
-            let res = image_decode(tidy);
-            res
-        },
-        Err(err) => {
-            let res = String::from("Error: unable to decode QR code");
-            res
-        }
-    }
+pub fn from_remote(url: &str, orig_cutoff: u32, new_cutoff: u32) -> Result<String, Box<dyn std::error::Error>> {
+    let qrcode = image_from_url(url)?;
+
+    //resize if nec
+    let tidy = image_dimensions(qrcode, orig_cutoff, new_cutoff);
+
+    let res = image_decode(tidy)?;
+    Ok(res)
 }
 
 //save image from url (keep original dimensions + aspect ratio)
-pub fn save_img(url: &str,path: &str,extension: ImageFormat){
-    let image = image_from_url(url);
-    image
-        .unwrap()
-        .save_with_format(path,extension)
-        .unwrap();
+pub fn save_img(url: &str,path: &str,extension: ImageFormat) -> 
+Result<(),Box<dyn std::error::Error>>{
+    let image = image_from_url(url)?;
+    image.save_with_format(path,extension)?;
+    Ok(())
 }
 
 //save image from url (resize image while maintaining original aspect ratio)
-pub fn save_img_resized(url: &str,orig_cutoff: u32,new_cutoff: u32,path: &str,extension: ImageFormat){
-    let image = image_from_url(url);
-    
-    let resized = image_dimensions(image.unwrap(),orig_cutoff,new_cutoff);
-    resized
-        .save_with_format(path,extension)
-        .unwrap();
+pub fn save_img_resized(
+    url: &str,orig_cutoff: u32,new_cutoff: u32,
+    path: &str,extension: ImageFormat
+) -> Result<(),Box<dyn std::error::Error>>{
+    let image = image_from_url(url)?;
+
+    //resize image
+    let resized = image_dimensions(image,orig_cutoff,new_cutoff);
+    resized.save_with_format(path,extension)?;
+    Ok(())
 }
